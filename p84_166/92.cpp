503c89652d9bf17977202c7b56f2cd81a6123ace
叉叉敌 <ievjai@126.com>
2022-03-02 17:31:22 +0800

    Create 92.cpp

---
+++ p84_166/92.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int & test01()
+{
+    int a = 10; //warning: reference to stack memory associated with local
+    return a;
+}
+
+int & test02()
+{
+    static int a = 10; // look good
+    return a;
+}
+
+
+int main()
+{
+    int b = test01();
+    cout << b << endl;
+    cout << b << endl;
+
+    int c = test02();
+    cout << c <<endl;
+    cout << c <<endl;
+    cout << c <<endl;
+}
+
+//output:
+//10 10 10 10 10 
